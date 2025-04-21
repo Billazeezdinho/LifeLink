b8b0e525c2a44c4e1be768ad2b631f3fc860e6eb
jcodemore <johnlegendry2@gmail.com>
2025-04-22 00:59:32 +0100

    correction

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,15 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
-// cloudinary.config({
-  // cloud_name: process.env.Cloud_name,
-  // api_key: process.env.API_key,
-  // api_secret: process.env.API_secret,
-  cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,  // Note the uppercase
-    api_key: process.env.API_KEY,        // Note the uppercase
-    api_secret: process.env.API_SECRET,  // Note the uppercase
-  
-  
+cloudinary.config({
+  cloud_name: process.env.Cloud_name,
+  api_key: process.env.API_key,
+  api_secret: process.env.API_secret,
   API_environment_variable: process.env.API_environment_variable
  });
 
